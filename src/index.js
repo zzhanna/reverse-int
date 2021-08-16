9b68dd4082f92ad1abe04145d0c409b6ac8c451a
zzhanna <foto1drug@gmail.com>
2021-08-17 01:38:38 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) n = n * (-1);
+    let numb = String(n).split('').reverse().join('');
+    return Number(numb);
 }
